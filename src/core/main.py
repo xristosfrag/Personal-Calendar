bf8f75840c29e813c0c490bb97a77b261feaa340
xristosfrag <37181592+xristosfrag@users.noreply.github.com>
2023-03-05 22:39:01 +0200

    Initial Commit

---
+++ src/core/main.py
@@ -0,0 +1,22 @@
+def main():
+    while True:
+        # Display the menu options
+        print("1. Add a new entry")
+        print("2. View previous entries")
+        print("3. Quit")
+
+        # Prompt the user for their choice
+        choice = input("Enter your choice: ")
+
+        # Call the appropriate function based on the user's choice
+        if choice == "1":
+            add_entry()
+        elif choice == "2":
+            view_entries()
+        elif choice == "3":
+            break
+        else:
+            print("Invalid choice. Please try again.")
+
+if __name__ == "__main__":
+    main()
